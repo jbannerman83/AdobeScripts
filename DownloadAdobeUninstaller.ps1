57b45181af6b29ee1c30dac83dbfbff74c2bb8ce
jbannerman83 <jason.bannerman@gmail.com>
2024-05-29 21:52:11 -0400

    Add files via upload

---
+++ DownloadAdobeUninstaller.ps1
@@ -0,0 +1,13 @@
+﻿#Create C:\Temp folder if it does not exist
+$path = "C:\Temp"
+If(!(test-path -PathType container $path))
+{
+      New-Item -ItemType Directory -Path $path
+}
+
+# URL and Destination
+$url = "https://ufile.io/2wigpqr8"
+
+# Download file
+#Invoke-WebRequest -Uri $url -OutFile $path #-Credential $credObject
+Start-BitsTransfer -Source $url -Destination $path
